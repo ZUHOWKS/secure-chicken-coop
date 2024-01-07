1dd387b41a75be82f12cd71532625531f2fe5a4a
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-07 15:31:22 +0100

    Replace file path by file absolute path

--- src/main.py
+++ src/main.py
@@ -2,21 +2,18 @@ from ia import chicken_detection as ChickenDetection
 from camera import picam as Picam
 from parameters import *
 import time
-
-
-print(ia_model_file)
-print(capture_loc)
+import os
 
 # Constants
-model = ChickenDetection.getIAModel(ia_model_file)
+model = ChickenDetection.getIAModel(os.path.abspath(ia_model_file))
 pi_camera = Picam.getCamera()
 
 # TODO: Capture image process from the camera
 pi_camera.start()
 
 time.sleep(2)
-pi_camera.capture_file(capture_loc)
-pred = ChickenDetection.predicate(model, capture_loc)
+pi_camera.capture_file(os.path.abspath(capture_loc))
+pred = ChickenDetection.predicate(model, os.path.abspath(capture_loc))
 
 pi_camera.stop()
 
