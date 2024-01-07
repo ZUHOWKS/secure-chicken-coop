97d6e0014968c310e039a49f6d71edb557e45d35
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-07 19:18:05 +0100

    Update code to loop

--- src/camera/picam.py
+++ src/camera/picam.py
@@ -1,4 +1,5 @@
 from picamera2 import Picamera2, Preview
+import os
 
 
 def getCamera():
@@ -6,3 +7,11 @@ def getCamera():
     camera_config = picam.create_preview_configuration()
     picam.configure(camera_config)
     return picam
+
+
+def takeCapture(picam, img):
+    try:
+        picam.capture_file(os.path.abspath(img))
+        return True
+    except:
+        return False
--- src/main.py
+++ src/main.py
@@ -7,16 +7,45 @@ import os
 # Constants
 model = ChickenDetection.getIAModel(os.path.abspath(ia_model_file))
 pi_camera = Picam.getCamera()
+on_magnets = True
+was_magnet_disabled = False
 
-# TODO: Capture image process from the camera
-pi_camera.start()
 
-time.sleep(2)
-# TODO: While loop
-pi_camera.capture_file(os.path.abspath(capture_loc))
-time.sleep(2)
-pred = ChickenDetection.predicate(model, os.path.abspath(capture_loc))
+# Main Function
+def checkChickenPresence(model, capture_loc):
+    retry = 0
+    chicken_is_present = False
+    while not (retry > 5 or chicken_is_present):
+        retry += 1
+        pred = ChickenDetection.predicate(model, os.path.abspath(capture_loc))
+        chicken_is_present = ChickenDetection.isChickenPredicated(pred)
 
-pi_camera.stop()
+        if not chicken_is_present:
+            time.sleep(0.25)
 
-print(ChickenDetection.isChickenPredicated(pred))
+    return chicken_is_present
+
+
+# Main Execute
+if __name__ == "__main__":
+    while True:
+
+        movementDetected = True  # TODO: MOVEMENT DETECTION
+
+        if movementDetected:
+            on_magnets = True
+            # TODO: ACTIVATE MAGNETS IF DISABLED
+
+            pi_camera.start()
+            time.sleep(2)
+
+            if Picam.takeCapture(pi_camera, capture_loc):
+                chickenIsPresent = checkChickenPresence(model, capture_loc)
+                if chickenIsPresent:
+                    # TODO: STOP MAGNETS
+                    on_magnets = False
+                    print("SCC-INFO: Disable Magnets...")
+
+            pi_camera.stop()
+
+            time.sleep(10)
