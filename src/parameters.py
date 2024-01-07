e7b0d827f32ae426ae8fc4596eab56d995a9765a
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-07 18:05:46 +0100

    Fix error captures not found

--- src/parameters.py
+++ src/parameters.py
@@ -1,3 +1,3 @@
 ia_model_file = "./resources/ia/models/mdl.pkl"
-capture_loc = "./resources/captures/latest_capture.jpg"
-chicken_test = "./resources/captures/chicken_test.jpg"
+capture_loc = "./resources/camera/captures/latest_capture.jpg"
+chicken_test = "./resources/camera/captures/chicken_test.jpg"
