51e38a8fb306c252a2c3002f7c8c9987a9ec90fa
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-07 14:51:39 +0100

    Added use of IA chicken detection model and picture

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from ia import chicken_detection as ChickenDetection
+from camera import picam as Picam
+from parameters import *
+import time
+
+# Constants
+model = ChickenDetection.getIAModel(ia_model_file)
+pi_camera = Picam.getCamera()
+
+# TODO: Capture image process from the camera
+pi_camera.start()
+
+time.sleep(2)
+pi_camera.capture_file(capture_loc)
+pred = ChickenDetection.predicate(model, capture_loc)
+
+pi_camera.stop()
+
+print(ChickenDetection.isChickenPredicated(pred))
