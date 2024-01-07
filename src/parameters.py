7524dd2bec934d621384620c12093276eedf1ec6
ZUHOWKS <jojo.vilard38@gmail.com>
2024-01-07 15:20:27 +0100

    Edit resources dir location

--- src/parameters.py
+++ src/parameters.py
@@ -1,2 +1,2 @@
-ia_model_file = "./../resources/ia/models/mdl.pkl"
-capture_loc = "../resources/captures/latest_capture.jpg"
+ia_model_file = "resources/ia/models/mdl.pkl"
+capture_loc = "resources/captures/latest_capture.jpg"
